96548076708feed725e90dff172e31a9c8edc1c7
Alex Wang <iamalex11303@gmail.com>
2023-12-23 17:28:07 -0500

    basic functionality, including the ability to move and shuffle the board, and check if the puzzle is solved

--- visual_search/puzzle.py
+++ visual_search/puzzle.py
@@ -1,5 +1,7 @@
 import utils
 
+from random import choice, seed
+
 class EightPuzzle:
   """
   Represents the 8-puzzle game.
@@ -10,6 +12,7 @@ class EightPuzzle:
   - state (list[int]): The current state of the puzzle.
   - statestr (str): A string representation of the current state.
   - adj_mask (list[int]): The adjacency mask for the blank space.
+  - z_index (int): The index of the blank space.
 
   Methods:
   - set(self, state: str) -> list[int]: Converts a string state to a list of integers.
@@ -21,7 +24,7 @@ class EightPuzzle:
   """
 
   goal_state: list[int] = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-  adj_masks: list[list[int]] = [
+  _adj_masks: list[list[int]] = [
     [0, 1, 0, 1, 0, 0, 0, 0, 0],
     [1, 0, 1, 0, 1, 0, 0, 0, 0],
     [0, 1, 0, 0, 0, 1, 0, 0, 0],
@@ -33,37 +36,38 @@ class EightPuzzle:
     [0, 0, 0, 0, 0, 1, 0, 1, 0]
   ]
 
-  def __init__(self, state: str):
+  def __init__(self, state: list[int]):
     """
     Initializes the EightPuzzle instance with the given state.
 
     Args:
-    - state (str): The initial state of the puzzle.
+    - state (list[int]): The initial state of the puzzle.
     """
     
-    self.state = self.set(state)
-    self.statestr = ''.join(str(i) for i in self.state)
-    self.adj_mask = self.adj_masks[self.state.index(0)]
+    self.set(state)
     
-  def set(self, state):
+  def set(self, state: list[int]):
     """
     Converts a string state to a list of integers.
 
     Args:
-    - state (str): The string representation of the state.
-
-    Returns:
-    - list[int]: The state as a list of integers.
+    - state (list[int]): The representation of the state.
     """
 
-    if isinstance(state, str):
-      state = ''.join(char for char in state if char.isdigit())
-      if len(state) != 9:
-        raise ValueError(f'filtered state {state} does not have 9 digits')
-      return utils.str2intlist(state, max_len=9)
-    
-    raise ValueError(f'given argument is not a str')
-
+    if not isinstance(state, list):
+      raise ValueError(f'given argument is not a list')
+    if len(state) != utils.num_tiles:
+      raise ValueError(f'state {state} does not have 9 digits')
+    for val in state:
+      if not isinstance(val, int):
+        raise ValueError(f'given values in list are not type int')
+    if set(state) != set(range(utils.num_tiles)):
+      raise ValueError(f'state does not have integers 0-8')
+      
+    self.state = state
+    self.statestr = ''.join(str(i) for i in self.state)
+    self.z_index = self.state.index(0)
+    self.adj_mask = self._adj_masks[self.z_index]
 
   def move(self, tile: int):
     """
@@ -72,7 +76,15 @@ class EightPuzzle:
     Args:
     - tile (int): The tile to move.
     """
-    # Implementation of move method...
+    
+    # Get the index of the tile and check for adjacency
+    t_index = self.state.index(tile)
+    if self.adj_mask[t_index]:
+      state = self.state.copy()
+      state[t_index], state[self.z_index] = state[self.z_index], state[t_index]
+      self.set(state)
+    else:
+      raise ValueError(f'the tile {tile} cannot be moved to the blank space')
 
   def is_solved(self):
     """
@@ -81,28 +93,36 @@ class EightPuzzle:
     Returns:
     - bool: True if the puzzle is solved, False otherwise.
     """
-    # Implementation of is_solved method...
+    return self.state == self.goal_state
 
   def print_board(self):
     """
     Prints the current state of the puzzle.
     """
 
-    print(self.state[0:3])
-    print(self.state[3:6])
-    print(self.state[6:9])
+    print(self.statestr[0:3])
+    print(self.statestr[3:6])
+    print(self.statestr[6:9])
 
-  def shuffle(self, k: int):
+  def shuffle(self, k: int, set_seed: int = None):
     """
     Shuffles the puzzle to generate a random solvable configuration.
 
     Args:
     - k (int): The number of random moves to perform.
+    - seed (int): The seed for RNG.
     """
-    # Implementation of shuffle method...
+    
+    if seed:
+      seed(set_seed)
+
+    for i in range(k):
+      options = [self.state[index] for index, value in enumerate(self.adj_mask) if value]
+      self.move(choice(options))
 
   def reset(self):
     """
     Resets the puzzle to its initial state.
     """
-    # Implementation of reset method...
+
+    self.set(self.goal_state.copy())
